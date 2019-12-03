e68a175575b1949e1968468cb5a559013b86a0c2
kberber <kenan.berber@32bit.com.tr>
2019-12-03 11:01:58 +0300

    First initialize

--- src/test/java/com/kberber/issuemanagement/IssueManagementApplicationTests.java
+++ src/test/java/com/kberber/issuemanagement/IssueManagementApplicationTests.java
@@ -1,12 +1,11 @@
 package com.kberber.issuemanagement;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class IssueManagementApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
